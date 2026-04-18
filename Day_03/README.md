1b6f62a96061a4c4903ea7656ef5bd8a1aa4755a
abdallah ashraf <a11b22d33@shadow.shadow>
2026-04-18 21:52:36 +0200

    Day 3 completed

---
+++ Day_03/README.md
@@ -0,0 +1,3 @@
+# Day 3: Static Analysis
+
+Extracted hardcoded flag using: `strings secret | grep FLAG`
---
+++ Day_03/secret.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    char flag[] = "FLAG{STATIC_ANALYSIS_MASTER_2026}";
+    printf("I have a secret, but I won't tell you.\n");
+    return 0;
+}
