95e119ccc3e446227192585573f41b88d29c0233
abdallah ashraf <a11b22d33@shadow.shadow>
2026-04-20 21:16:33 +0200

    feat: added dropper source code and compiled payload for Day 5

---
+++ Day_05/dropper.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    /* * DAY 5: PAYLOAD DISGUISE SIMULATION
+     * This binary simulates a malicious dropper.
+     * It does nothing when executed, but it is compiled 
+     * and renamed to 'vacation_photo.jpg' to bypass 
+     * surface-level user suspicion and file-extension filters.
+     */
+    return 0;
+}
